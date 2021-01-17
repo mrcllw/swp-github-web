8725936da684dda0f1879e5e8702374c84da008d
Marcello Lopes <mrcllw@hotmail.com>
2021-01-17 16:16:56 -0400

    Fix api url

--- src/config/http.js
+++ src/config/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const baseURL = 'http://swp-github-api.herokuapp.com/api'
+export const baseURL = 'https://swp-github-api.herokuapp.com/api'
 export const http = axios.create({
   baseURL
 })
